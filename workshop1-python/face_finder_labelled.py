087a2601a852c80e86c3865713729dcf3d7b825d
Anirudh Balaji <anirudhqazxswced+github@gmail.com>
2019-04-14 20:09:56 -0700

    Initial commit

---
+++ workshop1-python/face_finder_labelled.py
@@ -0,0 +1,67 @@
+import numpy as np
+import cv2 as cv
+rectangles = False
+face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_cascade = cv.CascadeClassifier("haarcascade_eye_tree_eyeglasses.xml")
+smile_cascade = cv.CascadeClassifier("haarcascade_smile.xml")
+nose_cascade = cv.CascadeClassifier("haarcascade_mcs_nose.xml")
+#smiley = cv.imread("smiley.jpg")
+catnose = cv.imread("catnose.jpg")
+print catnose.shape, catnose.shape[0], catnose.shape[1]
+grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+label = raw_input("Label name:")
+
+for name in os.listdir("images"):
+    img = cv.imread("images/" + name)
+    faces = face_cascade.detectMultiScale(grey, 1.3, 5)
+    for (x,y,w,h) in faces:
+        if rectangles:
+            cv.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
+        roi_gray = grey[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            if rectangles:
+                cv.rectangle(roi_color, (ex-10,ey-10), (ex-10+ew,ey-10), (0,0,0), 2)
+                cv.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,255,0), 2)
+        smiles = smile_cascade.detectMultiScale(roi_gray)
+        for (sx,sy,sw,sh) in smiles:
+            if rectangles:
+                cv.rectangle(roi_color, (sx,sy), (sx+sw,sy+sh), (0,0,255), 2)
+            #cv.addWeighted(smiley, 1, roi_color, 0, 0, roi_color)
+            #ss = smiley.shape
+            #`img[sy:sy+ss[0], sx:sx+ss[0]] = cv.addWeighted(smiley, 1, img[sy:sy+ss[0], sx:sx+ss[0]], 1, 0)
+            break # Only one smile per face.
+        noses = nose_cascade.detectMultiScale(roi_gray)
+        for (nx,ny,nw,nh) in noses:
+            print nx,ny,nw,nh
+            if rectangles:
+                cv.rectangle(roi_color, (nx,ny), (nx+nw,ny+nh), (255,255,0), 2)
+            # Move the nose a little so that it is centered onto the nose.
+            cn = catnose.shape
+            print cn
+            fx = y+ny-(cn[0]/2)#-(w/2)-(cn[0]/2)
+            fx2 = y+ny+nw+(cn[0]/2)#-(w/2)+(cn[0]/2)
+            fy = x+nx-(cn[0]/2)#-(h/2)-(cn[1]/2)
+            fy2 = x+nx+nh+(cn[0]/2)#-(h/2)+(cn[1]/2)
+            fx = max([0,fx])
+            fx2 = max([0,fx2])
+            fy = max([0,fy])
+            fy2 = max([0,fy2])
+            sl = img[fx:fx2, fy:fy2]
+            #cv.imshow("img", sl)
+            #cv.waitKey(0)
+            print sl.shape
+            # Slice out 
+            nn = cv.resize(catnose, (sl.shape[1], sl.shape[0]))
+            print nn.shape
+            #cv.imshow("img", nn)
+            #cv.waitKey(0)
+            img[fx:fx2, fy:fy2] = cv.addWeighted(nn, 1, sl, 1, 0)
+            break # Only one nose per face.
+    #nsm = cv.resize(smiley, img.shape[:-1])
+    #img = cv.addWeighted(img, 0., nsm, 1., 0.)
+    cv.imshow("img", img)
+    cv.imwrite("images/" + name + ".output.jpg", img)
+
+cv.destroyAllWindows()
---
+++ workshop1-python/workshop.py
@@ -0,0 +1,61 @@
+﻿from PIL import Image
+
+# RGB values for recoloring.
+
+darkBlue = (0, 51, 76)
+red = (217, 26, 33)
+lightBlue = (112, 150, 158)
+yellow = (252, 227, 166)
+
+# Import image.
+
+my_image = Image.open(raw_input("Picture name: ")) #change IMAGENAME to the path on your computer to the image you're using
+image_list = my_image.getdata() # each pixel is represented in the form (red value, green value, blue value, transparency). You don't need the fourth value.
+image_list = list(image_list) # Turns the sequence above into a list. The list can be iterated through in a loop.
+recolored = [] #list that will hold the pixel data for the new image.
+
+#YOUR CODE to loop through the original list of pixels and build a new list based on intensity should go here.
+for pixel in image_list:
+    # Calculate the intensity of the pixel, adjusting with a bias.
+    intensity = (pixel[0] + pixel[1] + pixel[2]) / 3.
+    # Based on the intensity color the new pixel.
+    if intensity <= 64:
+        recolored.append(darkBlue)
+    elif intensity <= 128:
+        recolored.append(lightBlue)
+    elif intensity <= 192:
+        recolored.append(yellow)
+    else:
+        recolored.append(red)
+
+# Fix pixels.
+fix_enable = raw_input("Fix pixels(y/n)? ") == "y"
+if fix_enable:
+    chunk_size = 3
+    chunks = [range(i, i+chunk_size) for i in range(0, len(recolored), chunk_size)]
+    for xs in chunks:
+        # Filter out indexes that are out of bounds.
+        xs = [i for i in xs if i < len(recolored)]
+        num_lb = sum([1 if recolored[i] == lightBlue else 0 for i in xs])
+        num_db = sum([1 if recolored[i] == darkBlue else 0 for i in xs])
+        num_r = sum([1 if recolored[i] == red else 0 for i in xs])
+        num_y = sum([1 if recolored[i] == yellow else 0 for i in xs])
+        if num_lb >= chunk_size/2+1:
+            for i in xs:
+                recolored[i] = lightBlue
+        elif num_db >= chunk_size/2+1:
+            for i in xs:
+                recolored[i] = darkBlue
+        elif num_r >= chunk_size/2+1:
+            for i in xs:
+                recolored[i] = red
+        elif num_y >= chunk_size/2+1:
+            for i in xs:
+                recolored[i] = yellow
+
+
+# Create a new image using the recolored list. Display and save the image.
+new_image = Image.new("RGB", my_image.size) #Creates a new image that will be the same size as the original image.
+new_image.putdata(recolored) #Adds the data from the recolored list to the image.
+new_image.show() #show the new image on the screen
+new_image.save("recolored.jpg", "jpeg") #save the new image as "recolored.jpg"
